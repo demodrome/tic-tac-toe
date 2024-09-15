9ef5c1eea906797cd4e3e44baf3b1bb2dfe305c5
Chris <167183247+demodrome@users.noreply.github.com>
2024-09-15 15:45:36 +0100

    feat: Start the game

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 import splash from "./splash.js";
+import gameController from "./gameController.js";
 
 const root = document.querySelector('[data-game-container]');
 const splashScreen = splash().render(root);
 
 splashScreen.startButton.addEventListener('click', () => {
-  // Start the game here
+  // Start the game
+  const game = gameController(
+    splashScreen.playerOneInputField.value,
+    splashScreen.playerTwoInputField.value
+  );
+  
+  // Wipe game container prior to rendering game view
+  root.innerHTML = "";
+  game.startGame();
 });
