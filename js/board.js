ca96967b591424827b84b5f2e463ea3c2ecbce51
Chris <167183247+demodrome@users.noreply.github.com>
2024-09-15 12:49:42 +0100

    feat: Add board marking functionality

--- js/board.js
+++ js/board.js
@@ -2,9 +2,9 @@
  * Handle interactions with the game board
  * @returns {Object} The game board API
  */
-function board() {
+const board = (function () {
   const board = [
-    ['', '', ''],
+    ['X', '', ''],
     ['', '', ''],
     ['', '', '']
   ];
@@ -17,9 +17,29 @@ function board() {
     return board;
   }
 
-  return {
-    getBoard
+  /**
+   * Place a mark on the board
+   * @param {Number} x The x position of the marker
+   * @param {Number} y The y position of the marker
+   * @param {String} marker The marker to place
+   */
+  function mark(x, y, marker) {
+    // Only mark if the position is empty and a marker is present
+    if (board[x][y]) {
+      throw new Error(`[${x},${y}] has already been marked.`);
+    }
+
+    if (!marker) {
+      throw new Error('No marker found.');
+    }
+    
+    board[x][y] = marker;
   }
-}
+
+  return {
+    getBoard,
+    mark
+  }
+})();
 
 export default board;
