551b8af71fe2fecf3ee520def36af2138afda88a
Chris <167183247+demodrome@users.noreply.github.com>
2024-09-15 15:12:11 +0100

    feat: Check for winner and update score

--- js/gameController.js
+++ js/gameController.js
@@ -19,7 +19,7 @@ function gameController(playerOneName, playerTwoName) {
     playerOne.getName(),
     playerTwo.getName()
   );
-  
+
   /**
    * Starts the game
    */
@@ -35,9 +35,67 @@ function gameController(playerOneName, playerTwoName) {
       board.mark(markPositionX, markPositionY, activePlayerMarker);
       view.update(board.getBoard());
 
-      // Switch the active player
-      activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
+      if (!checkForWin()) {
+        // Switch the active player
+        activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
+      } else {
+        activePlayer.addScore();
+        board.reset();
+        view.update(board.getBoard());
+        console.log(`${activePlayer.getName()} : ${activePlayer.getScore()}`);
+      }
     });
+  }
+
+  /**
+   * Checks every possible win condition of the board to see if a winning
+   * marker has been placed.
+   * @returns True or false if a win condition has been met
+   */
+  function checkForWin() {
+    const gameBoard = board.getBoard();
+
+    // Check the top row
+    if (gameBoard[0][0] !== '' && gameBoard[0][1] === gameBoard[0][0] && gameBoard[0][2] === gameBoard[0][0]) {
+      return true;
+    }
+
+    // Check the middle row
+    if (gameBoard[1][0] !== '' && gameBoard[1][1] === gameBoard[1][0] && gameBoard[1][2] === gameBoard[1][0]) {
+      return true;
+    }
+
+    // Check bottom row
+    if (gameBoard[2][0] !== '' && gameBoard[2][1] === gameBoard[2][0] && gameBoard[2][2] === gameBoard[2][0]) {
+      return true;
+    }
+
+    // Check left column
+    if (gameBoard[0][0] !== '' && gameBoard[1][0] === gameBoard[0][0] && gameBoard[2][0] === gameBoard[0][0]) {
+      return true;
+    }
+
+    // Check middle column
+    if (gameBoard[0][1] !== '' && gameBoard[1][1] === gameBoard[0][1] && gameBoard[2][1] === gameBoard[0][1]) {
+      return true;
+    }
+
+    // Check right column
+    if (gameBoard[0][2] !== '' && gameBoard[1][2] === gameBoard[0][2] && gameBoard[2][2] === gameBoard[0][2]) {
+      return true;
+    }
+
+    // Check diagonal - top left to bottom right
+    if (gameBoard[0][0] !== '' && gameBoard[1][1] === gameBoard[0][0] && gameBoard[2][2] === gameBoard[0][0]) {
+      return true;
+    }
+
+    // Check diagonal - bottom left to top right
+    if (gameBoard[2][0] !== '' && gameBoard[1][1] === gameBoard[2][0] && gameBoard[0][2] === gameBoard[2][0]) {
+      return true;
+    }
+
+    return false;
   }
 
   return {
